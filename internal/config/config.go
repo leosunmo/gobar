c96108334537bc15fded8216105455ca74eabad8
leo palmer sunmo <leo.sunmo@gmail.com>
2019-10-22 22:10:53 +1300

    tidy up config parsing

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,6 +9,21 @@ import (
 	"gopkg.in/ini.v1"
 )
 
+const (
+	Setuid uint32 = 1 << (12 - 1 - iota)
+	Setgid
+	Sticky
+	UserRead
+	UserWrite
+	UserExecute
+	GroupRead
+	GroupWrite
+	GroupExecute
+	OtherRead
+	OtherWrite
+	OtherExecute
+)
+
 // Config for the bar and its blocks
 var Config BarConfig
 
@@ -21,25 +36,29 @@ type BarConfig struct {
 // GlobalConfig is a global section of the ini conf file that
 // applies as defaults to all blocks or sets generic settings
 type GlobalConfig struct {
-	Command    string
-	SepWidth   int
-	Markup     string
-	Color      string
-	LabelColor string
-	IconFont   string
-	LabelFont  string
-	ValueFont  string
+	Command    string `ini:"command"`
+	SepWidth   int    `ini:"separator_block_width"`
+	Markup     string `ini:"markup"`
+	Color      string `ini:"color"`
+	LabelColor string `ini:"label_color"`
+	IconFont   string `ini:"icon_font"`
+	LabelFont  string `ini:"label_font"`
+	ValueFont  string `ini:"value_font"`
 }
 
 // BlockConfig is the configuration for a single Blocklet
 type BlockConfig struct {
-	Command    string
-	Markup     string
-	Color      string
-	LabelColor string
-	IconFont   string
-	LabelFont  string
-	ValueFont  string
+	Command    string `ini:"command"`
+	Interval   string `ini:"interval"`
+	Markup     string `ini:"markup"`
+	Color      string `ini:"color"`
+	LabelColor string `ini:"label_color"`
+	IconFont   string `ini:"icon_font"`
+	LabelFont  string `ini:"label_font"`
+	ValueFont  string `ini:"value_font"`
+	blockName  string
+	blockCmd   string
+	envVars    map[string]string
 }
 
 // ReadConfig reads the block config and returns BarConfig
@@ -49,11 +68,12 @@ func ReadConfig(configFile string) error {
 	}
 	cfg, err := ini.Load(configFile)
 	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to read file: %v", err)
 	}
-	Config.readSections(cfg)
-
+	err = Config.readSections(cfg)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -68,23 +88,39 @@ func findFont(fontName string) (string, error) {
 func (bc *BarConfig) readSections(cfg *ini.File) error {
 	var err error
 	// Read the global section first
-	gs := cfg.Section("DEFAULT")
-	err = gs.MapTo(bc.Global)
-	if err != nil {
-		return fmt.Errorf("failed to map global config to struct, err %s", err.Error())
+	gs := cfg.Section(ini.DEFAULT_SECTION)
+	if len(gs.Keys()) > 0 {
+		err = gs.MapTo(&bc.Global)
+		if err != nil {
+			return fmt.Errorf("failed to map global config to struct, err %s", err.Error())
+		}
 	}
 	for _, s := range cfg.Sections() {
-		if s.Name() != "DEFAULT" { // lazy but it works
+		if s.Name() != ini.DEFAULT_SECTION {
 			blockConfig := BlockConfig{}
-			err = s.MapTo(blockConfig)
+			err = s.MapTo(&blockConfig)
 			if err != nil {
 				return fmt.Errorf("failed to map block config to struct, err %s", err.Error())
 			}
-			// Check if the command is exectuable and if we need to use the global path
-			if ok := isExecutable(blockConfig.Command); !ok {
-				// command isn't excutable or doesn't exist, even after using global path prefix
-				fmt.Printf("Failed execute %s", blockConfig.Command)
+			// Grab the values and send them to the shell as env vars
+			blockConfig.envVars = s.KeysHash()
+			// Save the name of the section to the block
+			blockConfig.blockName = s.Name()
+			// If the 'command' section is empty, use the title as the command
+			if blockConfig.Command == "" {
+				blockConfig.blockCmd = blockConfig.blockName
+			} else {
+				blockConfig.blockCmd = blockConfig.Command
 			}
+			// Find the blocks executable by using global command
+			blockConfig.blockCmd = findExecutable(blockConfig.blockCmd)
+			// Check if the command is exectuable and if we need to use the global path
+			if ok := isExecutable(blockConfig.blockCmd); !ok {
+				// command isn't excutable or doesn't exist, even after using global path prefix
+				fmt.Printf("%s doesn't exist or isn't executable\n", blockConfig.blockCmd)
+			}
+
+			// Append to our list of Blocklets
 			bc.BlockConfigs = append(bc.BlockConfigs, blockConfig)
 		}
 	}
@@ -109,8 +145,8 @@ func findExecutable(command string) string {
 func isExecutable(command string) bool {
 	info, err := os.Stat(command)
 	if !os.IsNotExist(err) {
-		if info.Mode() == 0100755 { // It's executable by somebody
-			return true
+		if uint32(info.Mode().Perm())&UserExecute != 0 {
+			return true // It's executable by somebody
 		}
 	}
 	return false
