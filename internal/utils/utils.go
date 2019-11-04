e6bac5ba561dda01cc7112918aad0cb4a6b20859
leo palmer sunmo <leo.sunmo@gmail.com>
2019-11-04 19:55:58 +1300

    add utils package for now

---
+++ internal/utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import "time"
+
+// Truncate returns string in at length of l
+func Truncate(in string, l int) string {
+	if len([]rune(in)) <= l {
+		return in
+	}
+	return string([]rune(in)[:l-1]) + "⋯"
+}
+
+func Hms(d time.Duration) (h int, m int, s int) {
+	h = int(d.Hours())
+	m = int(d.Minutes()) % 60
+	s = int(d.Seconds()) % 60
+	return
+}
